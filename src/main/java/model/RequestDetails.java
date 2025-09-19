9c237fe305ca31c9714b82240358eb06882d5b8e
carolina.cepeda-v <carolina.cepeda-v@mail.escuelaing.edu.co>
2025-09-18 23:29:43 -0500

    fix: @Document(collection = "request_details")

--- src/main/java/model/RequestDetails.java
+++ src/main/java/model/RequestDetails.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
 import java.util.UUID;
-
+@Document(collection = "request_details")
 public class RequestDetails {
     @Id
     private String id;
