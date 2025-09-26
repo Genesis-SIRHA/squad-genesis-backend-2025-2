907eef4b55acae70904d9623acaabb1f9b2354b5
Marlio Charry <marlio.charry@sainapsis.com>
2025-09-26 01:17:13 -0500

    feat: doc and fixes for some dtos

--- src/main/java/edu/dosw/dto/CourseRequest.java
+++ src/main/java/edu/dosw/dto/CourseRequest.java
@@ -7,17 +7,18 @@ import java.util.List;
 /**
  * Represents a request to create or update a course.
  * Contains the course code, name, and list of groups.
+ *
+ * @param code The unique code that identifies the course. Cannot be blank.
+ * @param name The name of the course. Cannot be blank.
+ * @param groups List of groups associated with this course.
  */
 public record CourseRequest(
-    /** The unique code that identifies the course. Cannot be blank. */
     @NotBlank(message = "El código del curso es obligatorio")
     String code,
     
-    /** The name of the course. Cannot be blank. */
     @NotBlank(message = "El nombre del curso es obligatorio")
     String name,
     
-    /** List of groups associated with this course. */
     @Valid
     List<GroupRequest> groups
 ) {}
--- src/main/java/edu/dosw/dto/GroupRequest.java
+++ src/main/java/edu/dosw/dto/GroupRequest.java
@@ -7,45 +7,51 @@ import jakarta.validation.constraints.NotNull;
 
 /**
  * Represents a request to create or update a group.
- * Contains group details like code, professor, maxCapacity, and enrolled students.
+ * Contains group details like code, professor, capacity, and enrolled students.
+ *
+ * @param groupCode Unique code that identifies the group. Cannot be blank.
+ * @param abbreviation Abbreviation of the course. Cannot be blank.
+ * @param year Academic year. Cannot be blank.
+ * @param period Academic period. Cannot be blank.
+ * @param teacherId ID of the professor for this group. Cannot be blank.
+ * @param isLab Indicates if the group is a lab. Cannot be null.
+ * @param groupNum Group number. Must be greater than 0.
+ * @param maxCapacity Maximum capacity of the group. Must be greater than 0.
+ * @param enrolled Number of currently enrolled students. Cannot be negative.
  */
 public record GroupRequest(
-    /** Unique code that identifies the group. Cannot be blank. */
     @NotBlank(message = "El código del grupo es obligatorio")
     String groupCode,
     
-    /** Abbreviation of the course. Cannot be blank. */
     @NotBlank(message = "La abreviatura del curso es obligatoria")
     String abbreviation,
     
-    /** Academic year. Cannot be blank. */
     @NotBlank(message = "El año académico es obligatorio")
     String year,
     
-    /** Academic period. Cannot be blank. */
     @NotBlank(message = "El período académico es obligatorio")
     String period,
     
-    /** ID of the professor for this group. Cannot be blank. */
     @NotBlank(message = "El ID del profesor es obligatorio")
     String teacherId,
     
-    /** Indicates if the group is a lab. */
     @NotNull(message = "El indicador de laboratorio es obligatorio")
     Boolean isLab,
     
-    /** Group number. Must be greater than 0. */
     @Min(value = 1, message = "El número de grupo debe ser mayor a 0")
     int groupNum,
     
-    /** Maximum maxCapacity of the group. Must be greater than 0. */
     @Min(value = 1, message = "La capacidad debe ser mayor a 0")
     int maxCapacity,
     
-    /** Number of currently enrolled students. Cannot be negative. */
     @Min(value = 0, message = "El número de inscritos no puede ser negativo")
     int enrolled
 ) {
+    /**
+     * Converts this GroupRequest to a Group entity.
+     *
+     * @return a new Group entity populated with this request's data
+     */
     public Group toEntity() {
         Group group = new Group();
         group.setGroupCode(this.groupCode);
