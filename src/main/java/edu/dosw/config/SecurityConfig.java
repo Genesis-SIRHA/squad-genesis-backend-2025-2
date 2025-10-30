1601815b90da6fb23e5d429a262b9311a8972a41
Sofia Ariza <sofia.ariza-g@mail.escuelaing.edu.co>
2025-10-30 14:58:52 -0500

    chore(SecurityConfig): Update allowed origins for CORS

--- src/main/java/edu/dosw/config/SecurityConfig.java
+++ src/main/java/edu/dosw/config/SecurityConfig.java
@@ -65,7 +65,11 @@ public class SecurityConfig {
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
     configuration.setAllowedOrigins(
-        Arrays.asList("http://localhost:5173", "http://localhost:3000", "http://localhost:5174", "https://squad-genesis-frontend-2025-2.vercel.app"));
+        Arrays.asList(
+            "http://localhost:5173",
+            "http://localhost:3000",
+            "http://localhost:5174",
+            "https://squad-genesis-frontend-2025-2.vercel.app"));
     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
     configuration.setAllowCredentials(true);
